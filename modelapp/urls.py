d58fb6a3932e0d3949cde5b4d276fe063964260b
isxoqovbegzod <91113167+isxoqovbegzod@users.noreply.github.com>
2022-04-30 16:40:02 +0500

    product page updata

--- modelapp/urls.py
+++ modelapp/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', home, name='home'),
     path('categoriy/<int:id>/<str:cat_title>', subcategory_1, name='subcategory_1'),
     path('categoriy/<str:cat_title>/<str:subcategory1_name>/<int:id>', subcategory_2, name='subcategory_2'),
+    path('product/view/<str:product_model_name>/<int:id>', product_view, name='product_view')
 
 ]
 
--- modelapp/views.py
+++ modelapp/views.py
@@ -25,6 +25,11 @@ def subcategory_2(request, cat_title, subcategory1_name, id):
     return render(request, 'sub1category.html', context)
 
 
+def product_view(request, product_model_name, id):
+    category = Subcategory2.objects.filter(id=id)
+    context = {"categorys":category}
+    return render(request, 'product_view.html', context)
+
 
 
 
